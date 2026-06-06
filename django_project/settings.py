d2943bda2e276c4571d92239dd05338f7e527ce2
msayracode <msayracode@gmail.com>
2026-06-06 14:11:26 +0300

    Login and Logout System

--- django_project/settings.py
+++ django_project/settings.py
@@ -123,3 +123,10 @@ STATIC_URL = 'static/'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
+
+LOGIN_REDIRECT_URL = 'blog-home'
+
+# settings.py
+LOGOUT_ON_GET = True
+LOGOUT_REDIRECT_URL = 'login' # Çıkış yapınca otomatik login sayfasına atar
+
