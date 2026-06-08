747c6f5a484802adb3753bd9a5f79c7e6b709adf
msayracode <msayracode@gmail.com>
2026-06-08 09:53:58 +0300

    user Profile and Picture

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,5 @@ LOGOUT_ON_GET = True
 LOGOUT_REDIRECT_URL = 'login' # Çıkış yapınca otomatik login sayfasına atar
 LOGIN_URL = 'login'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
--- django_project/urls.py
+++ django_project/urls.py
@@ -20,6 +20,10 @@ from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from users import views as user_views
 
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
@@ -29,4 +33,7 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     # path('register/', user_views.register, name='register'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
