f1632bba3f1fb525a9722daa411388bad8bac675
msayracode <msayracode@gmail.com>
2026-06-09 19:29:14 +0300

    class based  view

--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,32 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 from django.http import HttpResponse
 from .models import Post
 
 # Create your views here.
-def home(request):
-    
+# class based view
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # default olarak blog/post_list.html altında arıyor. Biz route bu seklilde manuel olarak verdik
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+# function based view
+def home(request):    
     context = {
         'posts':Post.objects.all()
     }
     return render(request,'blog/home.html', context)
 
+
+# class based view
+class PostDetailView(DetailView):
+    model = Post
+        
+
 def about(request):
     # return HttpResponse('<h1>Blog About</h1>')
-
     return render(request, 'blog/about.html', {'title':'about'})
-
 
 
 # blog -> templates -> blog  templates.html is live
