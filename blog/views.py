b974c8ec11ea35b2ae7df10077a3ee68a34ce0d5
msayracode <msayracode@gmail.com>
2026-06-11 13:14:48 +0300

    class based view Create, Update, and Delete Posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
 from django.views.generic import (ListView,
                                   DetailView,
-                                  CreateView
+                                  CreateView,
+                                  UpdateView,
+                                  DeleteView
 )
 from django.http import HttpResponse
 from .models import Post
@@ -26,18 +30,54 @@ def home(request):
 class PostDetailView(DetailView):
     model = Post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title','content']
 
     def form_valid(self,form):
         form.instance.author = self.request.user
         return super().form_valid(form)
-        
+    
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
 
 def about(request):
     # return HttpResponse('<h1>Blog About</h1>')
     return render(request, 'blog/about.html', {'title':'about'})
+
+
+
+
+
+
+
+
 
 
 # blog -> templates -> blog  templates.html is live
