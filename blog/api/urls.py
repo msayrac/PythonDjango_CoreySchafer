8e91b869de288db7a82c77a4fd54180cbb4ef206
msayracode <msayracode@gmail.com>
2026-06-19 22:02:07 +0300

    PostDetailAPIView added

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from blog.api import views as api_views
+from blog.api.views import PostListCreateAPIView, PostDetailAPIView
 
 urlpatterns = [
-    path('posts/', api_views.PostListCreateAPIView.as_view(), name='api-post-list')
+    path('posts/', PostListCreateAPIView.as_view(), name='api-post-list'),
+    path('posts/<int:pk>/', PostDetailAPIView.as_view(), name='api-post-detail'),
 ]
--- blog/api/views.py
+++ blog/api/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from blog.models import Post
 from blog.api.serializers import PostSerializer
 from rest_framework import status # durum kodları için gereklidir
-
+from rest_framework.generics import get_object_or_404
 
 class PostListCreateAPIView(APIView):
     def get(self, request):
@@ -21,4 +21,25 @@ class PostListCreateAPIView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status = status.HTTP_400_BAD_REQUEST)
 
+class PostDetailAPIView(APIView):
+    def get_object(self,pk):
+        post_instance = get_object_or_404(Post, pk=pk)
+        return post_instance
+    
+    def get(self, request,pk):
+        post = self.get_object(pk=pk)
+        serializer = PostSerializer(instance = post)
+        return Response(serializer.data)
+    
+    def put(self,request, pk):
+        post = self.get_object(pk=pk)
+        serializer = PostSerializer(instance = post, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def delete(self, request, pk):
+        post = self.get_object(pk)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
